572b05f379ad7bbfe65392a2e430fe276ff43260
Matteo Soccal <soccal.matteo@gmail.com>
2025-01-21 09:41:09 +0100

    Row Mapper

--- src/main/java/com/demo/SpringJDBC/repo/StudentRepo.java
+++ src/main/java/com/demo/SpringJDBC/repo/StudentRepo.java
@@ -3,8 +3,11 @@ package com.demo.SpringJDBC.repo;
 import com.demo.SpringJDBC.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +33,30 @@ public class StudentRepo {
     }
 
     public List<Student> findAll() {
-        List<Student> students = new ArrayList<>();
-        return students;
+        String sql = "select * from student";
+
+        /*
+        RowMapper<Student> mapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Student s = new Student();
+                s.setRollNo(rs.getInt("rollno"));
+                s.setName(rs.getString("name"));
+                s.setMarks(rs.getInt("marks"));
+                return s;
+            }
+        };
+        return jdbc.query(sql, mapper);
+        */
+
+        // lambda version
+        return jdbc.query(sql, (ResultSet rs, int rowNum) -> {
+            Student s = new Student();
+            s.setRollNo(rs.getInt("rollno"));
+            s.setName(rs.getString("name"));
+            s.setMarks(rs.getInt("marks"));
+            return s;
+        });
+
     }
 }
--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,5 +1,5 @@
 create table student(
-rollno int primary key,
-name varchar(50),
-marks int
+    rollno int primary key,
+    name varchar(50),
+    marks int
 );
